b794ae1d0830f3820aa639df96764df5f84446b8
ThatOneGuy <161470944+AverageNormal@users.noreply.github.com>
2024-05-08 20:43:51 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+//Project Data
+console.log('Link Tree Website')
+console.log('Uploaded To Github')
+console.log('Project #01, Version 1.0')
+
+
+// Main JavaScript
+//Variable
+let inst;
+let discord;
+let tikTok;
+let gitHub;
+let solLearn;
+let scratch;
+let gDrive;
+let DDLC;
+let omori;
+
+// Redirect
+//Instagram
+inst = document.getElementById("ins");
+    inst.addEventListener("click", function() {
+        window.location.href = "instagram://user?username=comrade_blitzkrieg";
+    });
+
+//Discord
+discord = document.getElementById("disc");
+    discord.addEventListener("click",
+      function() {
+        window.location.href = "https://discord.com/invite/ENJNjDxN";
+      });
+
+//TikTok
+tikTok = document.getElementById("tik");
+    tikTok.addEventListener("click",
+      function() {
+        window.location.href = "tiktok://user/penyukaba";
+        });
+        
+//Github
+gitHub = document.getElementById("gHub");
+    gitHub.addEventListener("click",
+      function() {
+        window.location.href = "https://github.com/AverageNormal";
+      });
+      
+//SoloLearn
+solLearn = document.getElementById("sol");
+    solLearn.addEventListener("click",
+      function() {
+        window.location.href = "https://www.sololearn.com/profile/31949649/?ref=app";
+      });
+      
+//Scratch
+scratch = document.getElementById("scr");
+    scratch.addEventListener("click",
+    function() {
+        window.location.href = "https://scratch.mit.edu/users/ComradeBlux/";
+    });
+
+//GoogleDrive
+gDrive = document.getElementById("gDrive");
+    gDrive.addEventListener("click",
+    function() {
+      window.location.href = "https://drive.google.com/drive/folders/1-uM3kD1PGNNdSa9l7GjDNWWxXMEuXHLJ";
+    });
+
+//DDLC Droomp
+DDLC = document.getElementById("dDDLC");
+    DDLC.addEventListener("click", function() {
+        window.location.href = "https://www.youtube.com/watch?v=PsYXoao6qz4";
+    });
+    
+//Omori Droomp
+omori = document.getElementById("dOmori");
+    omori.addEventListener("click",
+      function() {
+        window.location.href = "https://www.youtube.com/watch?v=zE3iZ-b5L2M";
+      });
+      
+// End      
